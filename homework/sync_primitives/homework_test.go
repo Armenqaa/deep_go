23a62ad9ff366d45b66e68f0aacc66288df12d82
Armenqaa <130700638+Armenqaa@users.noreply.github.com>
2024-12-21 13:51:46 +0300

    Homework #13

---
+++ homework/sync_primitives/homework_test.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type RWMutex struct {
+	mu      *sync.Mutex
+	readers int
+}
+
+func (m *RWMutex) Lock() {
+	for {
+		m.mu.Lock()
+		if m.readers > 0 {
+			m.mu.Unlock()
+			continue
+		}
+	}
+}
+
+func (m *RWMutex) Unlock() {
+	m.mu.Unlock()
+}
+
+func (m *RWMutex) RLock() {
+	m.mu.Lock()
+	m.readers++
+	m.mu.Unlock()
+}
+
+func (m *RWMutex) RUnlock() {
+	m.mu.Lock()
+	m.readers--
+	m.mu.Unlock()
+}
+
+func TestRWMutexWithWriter(t *testing.T) {
+	var mutex RWMutex
+	mutex.Lock() // writer
+
+	var mutualExlusionWithWriter atomic.Bool
+	mutualExlusionWithWriter.Store(true)
+	var mutualExlusionWithReader atomic.Bool
+	mutualExlusionWithReader.Store(true)
+
+	go func() {
+		mutex.Lock() // another writer
+		mutualExlusionWithWriter.Store(false)
+	}()
+
+	go func() {
+		mutex.RLock() // another reader
+		mutualExlusionWithReader.Store(false)
+	}()
+
+	time.Sleep(time.Second)
+	assert.True(t, mutualExlusionWithWriter.Load())
+	assert.True(t, mutualExlusionWithReader.Load())
+}
+
+func TestRWMutexWithReaders(t *testing.T) {
+	var mutex RWMutex
+	mutex.RLock() // reader
+
+	var mutualExlusionWithWriter atomic.Bool
+	mutualExlusionWithWriter.Store(true)
+
+	go func() {
+		mutex.Lock() // another writer
+		mutualExlusionWithWriter.Store(false)
+	}()
+
+	time.Sleep(time.Second)
+	assert.True(t, mutualExlusionWithWriter.Load())
+}
+
+func TestRWMutexMultipleReaders(t *testing.T) {
+	var mutex RWMutex
+	mutex.RLock() // reader
+
+	var readersCount atomic.Int32
+	readersCount.Add(1)
+
+	go func() {
+		mutex.RLock() // another reader
+		readersCount.Add(1)
+	}()
+
+	go func() {
+		mutex.RLock() // another reader
+		readersCount.Add(1)
+	}()
+
+	time.Sleep(time.Second)
+	assert.Equal(t, int32(3), readersCount.Load())
+}
+
+func TestRWMutexWithWriterPriority(t *testing.T) {
+	var mutex RWMutex
+	mutex.RLock() // reader
+
+	var mutualExlusionWithWriter atomic.Bool
+	mutualExlusionWithWriter.Store(true)
+	var readersCount atomic.Int32
+	readersCount.Add(1)
+
+	go func() {
+		mutex.Lock() // another writer is waiting for reader
+		mutualExlusionWithWriter.Store(false)
+	}()
+
+	time.Sleep(time.Second)
+
+	go func() {
+		mutex.RLock() // another reader is waiting for a higher priority writer
+		readersCount.Add(1)
+	}()
+
+	go func() {
+		mutex.RLock() // another reader is waiting for a higher priority writer
+		readersCount.Add(1)
+	}()
+
+	time.Sleep(time.Second)
+
+	assert.True(t, mutualExlusionWithWriter.Load())
+	assert.Equal(t, int32(1), readersCount.Load())
+}
